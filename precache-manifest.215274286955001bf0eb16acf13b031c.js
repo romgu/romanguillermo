cc10ed8c65cd98c203ba6f942e1c07a869ee0b33
romgu <gmarca20@gmail.com>
2019-06-07 20:02:49 -0300

    Updates

---
+++ precache-manifest.215274286955001bf0eb16acf13b031c.js
@@ -0,0 +1,246 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8d2afe8ac17bdc4fa7380c69dc8b407",
+    "url": "/romanguillermo/index.html"
+  },
+  {
+    "revision": "d3fd9c65a1d00ffa67e7",
+    "url": "/romanguillermo/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "11d676ef9b0bb5c0ba12",
+    "url": "/romanguillermo/static/css/main.fbdc427e.chunk.css"
+  },
+  {
+    "revision": "d3fd9c65a1d00ffa67e7",
+    "url": "/romanguillermo/static/js/2.e81e04b4.chunk.js"
+  },
+  {
+    "revision": "11d676ef9b0bb5c0ba12",
+    "url": "/romanguillermo/static/js/main.8281bb23.chunk.js"
+  },
+  {
+    "revision": "1f983329b44eb21cf5fd",
+    "url": "/romanguillermo/static/js/runtime~main.2f833bc2.js"
+  },
+  {
+    "revision": "336a1274368c2d85184ccb3ed3c2d501",
+    "url": "/romanguillermo/static/media/1.336a1274.jpg"
+  },
+  {
+    "revision": "7b0de61619cd7255ff6d71d70088ccec",
+    "url": "/romanguillermo/static/media/1.7b0de616.jpg"
+  },
+  {
+    "revision": "92e41e98b93ac83445db8a8aea6651d7",
+    "url": "/romanguillermo/static/media/1.92e41e98.jpg"
+  },
+  {
+    "revision": "180bbd5c33f99d6b3992d6c3793ebe68",
+    "url": "/romanguillermo/static/media/10.180bbd5c.jpg"
+  },
+  {
+    "revision": "4a4cf88d073b847feda528c7e0c49ddc",
+    "url": "/romanguillermo/static/media/10.4a4cf88d.jpg"
+  },
+  {
+    "revision": "8f6c759772ae5b756586d9a8b1f87f73",
+    "url": "/romanguillermo/static/media/10.8f6c7597.jpg"
+  },
+  {
+    "revision": "0d148942ee33408be635390a6d63395f",
+    "url": "/romanguillermo/static/media/11.0d148942.jpg"
+  },
+  {
+    "revision": "444a4cbfc03c42961fdad432f763e7f5",
+    "url": "/romanguillermo/static/media/11.444a4cbf.jpg"
+  },
+  {
+    "revision": "b7096f41b958885df5b788e675c41e4a",
+    "url": "/romanguillermo/static/media/11.b7096f41.jpg"
+  },
+  {
+    "revision": "149ccc51782ecd4b1eb17a8b9b8eb0c0",
+    "url": "/romanguillermo/static/media/12.149ccc51.jpg"
+  },
+  {
+    "revision": "46584e5feed5b1ddfe561cdbbcd71579",
+    "url": "/romanguillermo/static/media/12.46584e5f.jpg"
+  },
+  {
+    "revision": "55836d442abc56e397039db77597c04a",
+    "url": "/romanguillermo/static/media/12.55836d44.jpg"
+  },
+  {
+    "revision": "42386ca272a7b973dff42644ba72b333",
+    "url": "/romanguillermo/static/media/13.42386ca2.jpg"
+  },
+  {
+    "revision": "54c70a8603eba9d1c0bd1e0d077d9348",
+    "url": "/romanguillermo/static/media/13.54c70a86.jpg"
+  },
+  {
+    "revision": "6dcf99492d18e383cc3af61516390511",
+    "url": "/romanguillermo/static/media/13.6dcf9949.jpg"
+  },
+  {
+    "revision": "27e1e6195a81fe145c55661a07234ca6",
+    "url": "/romanguillermo/static/media/14.27e1e619.jpg"
+  },
+  {
+    "revision": "60fc4704efb4c1592fe4e32c59245c95",
+    "url": "/romanguillermo/static/media/14.60fc4704.jpg"
+  },
+  {
+    "revision": "e1b565562dde03eca660ed6ebcefd0f2",
+    "url": "/romanguillermo/static/media/14.e1b56556.jpg"
+  },
+  {
+    "revision": "1385195bd91640962339c2659fda1fec",
+    "url": "/romanguillermo/static/media/15.1385195b.jpg"
+  },
+  {
+    "revision": "3b00547c7ccd65b6c2e5d92bdc885497",
+    "url": "/romanguillermo/static/media/15.3b00547c.jpg"
+  },
+  {
+    "revision": "e899bd949e9d055cd4f317e0e7eb55e3",
+    "url": "/romanguillermo/static/media/15.e899bd94.jpg"
+  },
+  {
+    "revision": "692b870a27d7b6fcc1ba64e589988bfa",
+    "url": "/romanguillermo/static/media/16.692b870a.jpg"
+  },
+  {
+    "revision": "d0f99ca6d2b7d7a3a9e53af64afdde8e",
+    "url": "/romanguillermo/static/media/17.d0f99ca6.jpg"
+  },
+  {
+    "revision": "a96f34ff18e96c68cb88b49460f06636",
+    "url": "/romanguillermo/static/media/19.a96f34ff.jpg"
+  },
+  {
+    "revision": "2c43f8f14ccc46822e1a2a0a3b2affa2",
+    "url": "/romanguillermo/static/media/2.2c43f8f1.jpg"
+  },
+  {
+    "revision": "2fcc0cb8255e119ed7dc30aa5513430b",
+    "url": "/romanguillermo/static/media/2.2fcc0cb8.jpg"
+  },
+  {
+    "revision": "d5daa7ee9aca584b50f994640ad53ae0",
+    "url": "/romanguillermo/static/media/2.d5daa7ee.jpg"
+  },
+  {
+    "revision": "fb46cdb34674c4f1608dd021ea654133",
+    "url": "/romanguillermo/static/media/20.fb46cdb3.jpg"
+  },
+  {
+    "revision": "262e26eae93c4ca83bb5a24eb6df5275",
+    "url": "/romanguillermo/static/media/21.262e26ea.jpg"
+  },
+  {
+    "revision": "56cec4f53880e60765fc70dee8bd547a",
+    "url": "/romanguillermo/static/media/22.56cec4f5.jpg"
+  },
+  {
+    "revision": "50a8d01ded86f84283f725a20adc316d",
+    "url": "/romanguillermo/static/media/23.50a8d01d.jpg"
+  },
+  {
+    "revision": "0c74a929a8d82f4c28a6e3a72adfe741",
+    "url": "/romanguillermo/static/media/3.0c74a929.jpg"
+  },
+  {
+    "revision": "679f075cbba2bd60cb2bb4cc836a4e8b",
+    "url": "/romanguillermo/static/media/3.679f075c.jpg"
+  },
+  {
+    "revision": "e44183eaf7a604459bfd9b3e102ddecb",
+    "url": "/romanguillermo/static/media/3.e44183ea.jpg"
+  },
+  {
+    "revision": "48880be17578d65cdeca99c98c3eb009",
+    "url": "/romanguillermo/static/media/4.48880be1.jpg"
+  },
+  {
+    "revision": "90d4b926981cfef8589865adeadedaa5",
+    "url": "/romanguillermo/static/media/4.90d4b926.jpg"
+  },
+  {
+    "revision": "d501aadbcd85917408265c5059aa975b",
+    "url": "/romanguillermo/static/media/4.d501aadb.jpg"
+  },
+  {
+    "revision": "0653cfc2d136f4de436db86279901679",
+    "url": "/romanguillermo/static/media/5.0653cfc2.jpg"
+  },
+  {
+    "revision": "ccea7a3b2919213b2d7a849927dcfcf2",
+    "url": "/romanguillermo/static/media/5.ccea7a3b.jpg"
+  },
+  {
+    "revision": "f3196df7766f6ea2da980811c5b73c70",
+    "url": "/romanguillermo/static/media/5.f3196df7.jpg"
+  },
+  {
+    "revision": "1be1e1ba3f809ea1605704bff9f9ab5d",
+    "url": "/romanguillermo/static/media/6.1be1e1ba.jpg"
+  },
+  {
+    "revision": "bc2e55ea8c1603d291ca2c14295a86b7",
+    "url": "/romanguillermo/static/media/6.bc2e55ea.jpg"
+  },
+  {
+    "revision": "f5bc1a94e1e526c2ccfe46b3528e6c91",
+    "url": "/romanguillermo/static/media/6.f5bc1a94.jpg"
+  },
+  {
+    "revision": "271c78ec86e4c7194a7af5f9f43c2fbe",
+    "url": "/romanguillermo/static/media/7.271c78ec.jpg"
+  },
+  {
+    "revision": "a4b020ba36f287640504aa7301ab31c5",
+    "url": "/romanguillermo/static/media/7.a4b020ba.jpg"
+  },
+  {
+    "revision": "f0da40606bc133d4981b67e4defcd634",
+    "url": "/romanguillermo/static/media/7.f0da4060.jpg"
+  },
+  {
+    "revision": "6f6119d01a7438738e4a2c2a84db71fe",
+    "url": "/romanguillermo/static/media/8.6f6119d0.jpg"
+  },
+  {
+    "revision": "87c45d20861aed08b54daa1ea4dcd160",
+    "url": "/romanguillermo/static/media/8.87c45d20.jpg"
+  },
+  {
+    "revision": "8ecdec46c4dd7ab3707dff70592d5d55",
+    "url": "/romanguillermo/static/media/8.8ecdec46.jpg"
+  },
+  {
+    "revision": "5b6462e1fa0ad269a29d553f75421016",
+    "url": "/romanguillermo/static/media/9.5b6462e1.jpg"
+  },
+  {
+    "revision": "651d8029784a50e0515b36f53eda5ace",
+    "url": "/romanguillermo/static/media/9.651d8029.jpg"
+  },
+  {
+    "revision": "efb1b4bbe6e19b1702afba891dadae24",
+    "url": "/romanguillermo/static/media/9.efb1b4bb.jpg"
+  },
+  {
+    "revision": "9f513ba15453fc10482df95fb72ae7d5",
+    "url": "/romanguillermo/static/media/Content1.9f513ba1.jpg"
+  },
+  {
+    "revision": "0f6e082f42f5247d82c3ade9c2a8f928",
+    "url": "/romanguillermo/static/media/Gilroy-ExtraBold.0f6e082f.otf"
+  },
+  {
+    "revision": "c62aded729bf7146d491275e5019d7fc",
+    "url": "/romanguillermo/static/media/Gilroy-Light.c62aded7.otf"
+  }
+]);
